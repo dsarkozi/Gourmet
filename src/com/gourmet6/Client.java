b12fae0d93c2fdc53bcfe1c9b6ccda057e74510a
Nightcore <yami.hell@gmail.com>
2013-04-23 13:01:50 +0200

    Client up

--- src/com/gourmet6/Client.java
+++ src/com/gourmet6/Client.java
@@ -12,27 +12,33 @@ public class Client {
 		this.phone = phone;
 	}
 	
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email)
+	{
 		this.email = email;
 	}
 
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 
-	public String getPhone() {
+	public String getPhone() 
+	{
 		return phone;
 	}
 
-	public void setPhone(String phone) {
+	public void setPhone(String phone) 
+	{
 		this.phone = phone;
 	}
 	
