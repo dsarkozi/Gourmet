16c91808971d38bd4398cb4cbf84cb3dd1123336
Vineuvall <quentin.meyers@student.uclouvain.be>
2013-04-23 13:56:58 +0200

    Debut interface

--- src/com/gourmet6/MainGourmet.java
+++ src/com/gourmet6/MainGourmet.java
@@ -3,6 +3,7 @@ package com.gourmet6;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainGourmet extends Activity
 {
@@ -12,6 +13,21 @@ public class MainGourmet extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.yes_button_connection).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						login();
+					}
+				});
+		findViewById(R.id.no_button_connection).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						//TODO
+					}
+				});
 	}
 
 	@Override
@@ -20,6 +36,10 @@ public class MainGourmet extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void login(){
+		setContentView(R.layout.activity_login);
 	}
 
 }
