6ef778c5e3e36dc38e0341aca10ee0a1592bc1f6
David Sarkozi <david.sarkozi@student.uclouvain.be>
2013-04-23 13:26:16 +0200

    Location stuff

--- src/com/gourmet6/MainGourmet.java
+++ src/com/gourmet6/MainGourmet.java
@@ -1,17 +1,84 @@
 package com.gourmet6;
 
+import android.location.*;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class MainGourmet extends Activity
 {
+	private String[] towns;
+	private String currentTown;
+	private String[] restaurants;
+	private Location location = null;
+	
+	public Location getDeviceLocation()
+	{
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locationListener = new LocationListener()
+		{
+			public void onLocationChanged(Location location)
+			{
+				// Called when a new location is found by the network location provider.
+				MainGourmet.this.location = location;
+		    }
+
+		    public void onStatusChanged(String provider, int status, Bundle extras)
+		    {
+		    	
+		    }
+
+		    public void onProviderEnabled(String provider)
+		    {
+		    	
+		    }
+
+		    public void onProviderDisabled(String provider)
+		    {
+		    	
+		    }
+		}
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+		while (location == null)
+		{
+			
+		}
+		locationManager.removeUpdates(locationListener);
+		return loc
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locationListener = new LocationListener()
+		{
+			public void onLocationChanged(Location location)
+			{
+				// Called when a new location is found by the network location provider.
+				MainGourmet.this.location = location;
+		    }
+
+		    public void onStatusChanged(String provider, int status, Bundle extras)
+		    {
+		    	
+		    }
+
+		    public void onProviderEnabled(String provider)
+		    {
+		    	
+		    }
+
+		    public void onProviderDisabled(String provider)
+		    {
+		    	
+		    }
+		}
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+		//locationManager.removeUpdates(locationListener);
+		setContentView(R.layout.activity_login);
 	}
 
 	@Override
