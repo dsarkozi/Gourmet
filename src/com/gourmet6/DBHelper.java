743287e81a44915367b93ea9a036a5cd60dcd4ea
Lena <vicky.peschke@gmail.com>
2013-04-30 22:58:33 +0200

    DBHelper : version provisoire qui ne marche pas encore

--- src/com/gourmet6/DBHelper.java
+++ src/com/gourmet6/DBHelper.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import com.myfirstproject.CursorFactory;
+
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -17,12 +20,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
 	private static final String DB_PATH = "/data/data/com.gourmet6/databases/";
-	private static final String DB_NAME = "gourmet3_sqlite";
+	private static final String DB_NAME = "gourmet3.sqlite";
 	private static final String DB_FULL_PATH = DB_PATH + DB_NAME;
 	public static final int DB_VERSION = 1;
 	
-	public SQLiteDatabase ourDB;
-	public final Context ourContext;
+	// attribut private dans SQLiteOpenHelper : private SQLiteDatabase mDatabase = null
+	private SQLiteDatabase ourDB;
+	// private SQLiteDatabase mDatabase;
+	// attribut private dans SQLiteOpenHelper : private Context mContext;
+	private final Context ourContext; // référence au contexte utilisé
 
 	/**
 	 * @param context
@@ -31,26 +37,12 @@ public class DBHelper extends SQLiteOpenHelper {
 	{
 		super(context, DB_NAME, null, DB_VERSION);
 		this.ourContext = context;
-		if(checkDB()) {
-			// the DB already exists
-			return;
-		} else
-		{
-			this.getReadableDatabase();
-			try
-			{
-				copyDB();
-			} catch (IOException e)
-			{
-				throw new Error("Error copying database at initialisation");
-			}
-		}
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db)
 	{
-		// do nothing
+		createDB();
 	}
 
 	@Override
@@ -58,23 +50,12 @@ public class DBHelper extends SQLiteOpenHelper {
 	{
 		// do nothing
 	}
-
-	@Override
-	public synchronized void close()
-	{
-
-		if (ourDB != null)
-		{
-			ourDB.close();
-		}
-		super.close();
-
-	}
 	
 	/*
 	 * Creates an empty DB on the system and rewrites it with our own.
+	 * If the DB already exists, it does nothing.
 	 */
-	public void createDB() throws IOException
+	private void createDB()
 	{
 		if (!checkDB())
 		{
@@ -85,9 +66,10 @@ public class DBHelper extends SQLiteOpenHelper {
 			}
 			catch (IOException e)
 			{
-				throw new Error("Error copying database");
+				System.err.println("Error copying database at creation");
 			}
 		}
+		ourDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null, SQLiteDatabase.CREATE_IF_NECESSARY|SQLiteDatabase.OPEN_READWRITE);
 	}
 	
 	/*
@@ -138,4 +120,14 @@ public class DBHelper extends SQLiteOpenHelper {
 		myOutput.close();
 		myInput.close();
 	}
+	
+	/*
+	 * Opens the DB in read and write mode
+	 * @param mode must be SQLiteDatabase.OPEN_READONLY or SQLiteDatabase.OPEN_READWRITE
+	 */
+/*	public void openDB(int mode) throws SQLException, IOException
+	{
+		this.createDB();
+		ourDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null, mode);
+	}*/
 }
