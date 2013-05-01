d56a4a4565d290b63b1c5cfb49a5406647dd1836
David Sarkozi <david.sarkozi@student.uclouvain.be>
2013-05-02 00:30:50 +0200

    Exception Dialog: Added stack trace

--- src/com/gourmet6/ExceptionFragment.java
+++ src/com/gourmet6/ExceptionFragment.java
@@ -12,16 +12,20 @@ import android.support.v4.app.DialogFragment;
 public class ExceptionFragment extends DialogFragment
 {
 
+	private static final String IS_CAUGHT = "isCaught";
+	private static final String STACK_TRACE = "stackTrace";
+	
 	public ExceptionFragment()
 	{
 		// Required empty public constructor
 	}
 	
-	public static ExceptionFragment getInstance(boolean isCaught)
+	public static ExceptionFragment getInstance(boolean isCaught, String stackTrace)
 	{
 		ExceptionFragment fragment = new ExceptionFragment();
 		Bundle args = new Bundle();
-		args.putBoolean("isCaught", isCaught);
+		args.putBoolean(IS_CAUGHT, isCaught);
+		args.putString(STACK_TRACE, stackTrace);
 		fragment.setArguments(args);
 		return fragment;
 	}
@@ -30,10 +34,12 @@ public class ExceptionFragment extends DialogFragment
 	public Dialog onCreateDialog(Bundle savedInstanceState)
 	{
 		super.onCreateDialog(savedInstanceState);
-		boolean isCaught = this.getArguments().getBoolean("isCaught", false);
+		boolean isCaught = this.getArguments().getBoolean(IS_CAUGHT, false);
+		String stackTrace = this.getArguments().getString(STACK_TRACE);
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		if (isCaught) builder.setTitle(R.string.caught_exception);
 		else builder.setTitle(R.string.uncaught_exception);
+		builder.setMessage(stackTrace);
 		builder.setPositiveButton(android.R.string.ok, null);
 		return builder.create();
 	}
