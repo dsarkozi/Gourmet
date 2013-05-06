fa8fc43e6232c860245ca5c7ba1ee48143229efa
Vineuvall <quentin.meyers@student.uclouvain.be>
2013-05-06 11:10:12 +0200

    TimeTable

--- src/com/gourmet6/TimeTable.java
+++ src/com/gourmet6/TimeTable.java
@@ -1,6 +1,11 @@
 package com.gourmet6;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import android.annotation.SuppressLint;
 
 public class TimeTable {
 	
@@ -13,7 +18,37 @@ public class TimeTable {
 	}
 	
 	public TimeTable(String openTime, String closingTime){
-		//TODO
+		this.openTime = parseHour(openTime);
+		this.closingTime = parseHour(closingTime);
+	}
+	
+	/**
+	 * 
+	 * @param date
+	 * @return Cette fonction transforme un String jour en une hour GregorianCalendar.
+	 * Cette fonction supporte 1 format :
+	 * hh:mm
+	 * Si ces formats ne sont pas respectes, ou qu'elle ne reussit pas la conversion, elle renvoit null.
+	 */
+	@SuppressLint("SimpleDateFormat")
+	public GregorianCalendar parseHour(String hour){
+		SimpleDateFormat ourFormat;
+		TimeZone timezone = TimeZone.getDefault();
+		GregorianCalendar cal = null;
+		if(hour.contains(":") && (!hour.contains("/") || !hour.contains("-"))){
+			ourFormat = new SimpleDateFormat("hh:mm");
+		}
+		else return cal;
+		try{
+			cal = new GregorianCalendar();
+			cal.setTime(ourFormat.parse(hour));
+			cal.setTimeZone(timezone);
+		}
+		catch (ParseException e){
+			System.out.println("Error encoding the date : "+e);
+			e.printStackTrace();
+		}
+		return cal;
 	}
 	
 	public GregorianCalendar getOpenTime() {
@@ -23,7 +58,7 @@ public class TimeTable {
 		this.openTime = openTime;
 	}
 	public void setOpenTime(String openTime){
-		//TODO
+		this.openTime = parseHour(openTime);
 	}
 	public GregorianCalendar getClosingTime() {
 		return this.closingTime;
@@ -32,7 +67,7 @@ public class TimeTable {
 		this.closingTime = closingTime;
 	}
 	public void setClosingTime(String closingTime){
-		//TODO
+		this.closingTime = parseHour(closingTime);
 	}
 
 }
