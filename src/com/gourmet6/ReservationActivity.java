b0d49f9903a13189401fb6e8c1fb5baf62616a5e
ydebast <yves.debast@student.uclouvain.be>
2013-05-10 00:58:24 +0200

    menu dans reservation

--- src/com/gourmet6/ReservationActivity.java
+++ src/com/gourmet6/ReservationActivity.java
@@ -194,24 +194,14 @@ public class ReservationActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.reservation, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
+		Intent clientGo = new Intent(ReservationActivity.this, ClientActivity.class);
+		startActivity(clientGo);
 		return super.onOptionsItemSelected(item);
 	}
 
