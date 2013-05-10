bc7b1cad9bcd0ae20e5e8122a7ba04b14936328e
Nightcore <yami.hell@gmail.com>
2013-05-10 12:01:59 +0200

    haha

--- src/com/gourmet6/DishMenuActivity.java
+++ src/com/gourmet6/DishMenuActivity.java
@@ -20,7 +20,6 @@ import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class DishMenuActivity extends Activity {
