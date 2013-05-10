7a34038d08572535670f4621c5aa780ba7218b86
David Sarkozi <david.sarkozi@student.uclouvain.be>
2013-05-10 02:54:52 +0200

    Register

--- src/com/gourmet6/LoginActivity.java
+++ src/com/gourmet6/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteException;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -409,6 +410,7 @@ public class LoginActivity extends Activity
 			catch (SQLiteException e)
 			{
 				ExceptionHandler.caughtException(LoginActivity.this, e);
+				return false;
 			}
 			
 			if (check)
@@ -457,22 +459,21 @@ public class LoginActivity extends Activity
 
 			try
 			{
-				// Simulate network access.
-				Thread.sleep(2000);
+				db = new DBHandler(LoginActivity.this);
+				db.addClient(mEmail, mName, mPassword, mPhone);
 			}
-			catch (InterruptedException e)
+			catch (SQLiteException e)
+			{
+				ExceptionHandler.caughtException(LoginActivity.this, e);
+				return false;
+			}
+			catch (SQLException e)
 			{
 				return false;
 			}
-
-			for (String credential : DUMMY_CREDENTIALS)
+			finally
 			{
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail))
-				{
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
+				db.close();
 			}
 
 			// TODO: register the new account here.
@@ -492,7 +493,7 @@ public class LoginActivity extends Activity
 			else
 			{
 				mEmailView
-						.setError(getString(R.string.error_credentials_no_match));
+						.setError(getString(R.string.error_email_exists));
 				mEmailView.requestFocus();
 			}
 		}
