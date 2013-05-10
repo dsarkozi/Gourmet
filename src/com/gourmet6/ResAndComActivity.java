aa0935849884f35f123026dc65a8940a6aeab7d8
Nightcore <yami.hell@gmail.com>
2013-05-10 23:16:18 +0200

    working but need to be feed

--- src/com/gourmet6/ResAndComActivity.java
+++ src/com/gourmet6/ResAndComActivity.java
@@ -31,6 +31,7 @@ public class ResAndComActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_res_and_com);
+		setTitle(R.string.rescom);
 		dbHand = new DBHandler(this);
 		
 		g = (Gourmet)getApplication();
